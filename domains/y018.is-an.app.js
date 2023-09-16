e8847f27e35a94a3bd2bb099bbfedbe7db3664e6
bananaboiii-53 <127419046+bananaboiii-53@users.noreply.github.com>
2023-09-16 09:38:32 -0500

    Update y018.is-an.app.js

--- domains/y018.is-an.app.js
+++ domains/y018.is-an.app.js
@@ -1,19 +1,15 @@
 // for more details watch the file `./domains.d.ts`
 
 addSubDomain({
-  description: '...', // describe your project in this field
-  domain: '1bt.uk', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'foobar', // desired subdomain name
+  description: 'Gamsssss', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'y018', // desired subdomain name
   owner: {
-    repo: '<https://URL/to/the/repository/with/your/project/sources/if/it/is/public>',
-    email: '<your-public@email.address>',
+    repo: '<https://replit.com/@Oscar-LL/Html-test>',
+    email: '<y018@gmail.com>',
   },
   record: {
-    CNAME: '<cname-domain>', // e.g.: <your-github-account>.github.io
-    TXT: ['list', 'of', 'required', 'txt', 'records'],
-    A: ['list', 'of', 'IPv4', 'addresses', 'like', 'a', '127.0.0.1'],
-    AAAA: ['list', 'of', 'IPv6', 'addresses', 'like', 'a', '::1'],
-    NS: ['list', 'of', 'nameservers'],
+    CNAME: '<oscar-ll.repl.co>', // e.g.: <your-github-account>.github.io
   },
   //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
                   // should provide valid a SSL certificate and protection CF will be disabled
